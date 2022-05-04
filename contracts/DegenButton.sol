0477dcd2a5dab52b0a49fab225e2f8801ed1090d
JBLee <brianlee2795@gmail.com>
2022-05-04 00:37:45 -0700

    Update button to keep track of more parameters
    This should enable us to define allocations for the house and for
    seeding the next round.

--- contracts/DegenButton.sol
+++ contracts/DegenButton.sol
@@ -2,44 +2,101 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenButton {
-    address private lastSeen; // initialized to 0x0 address
-    uint lastBlockNumber;
-    uint fee; // denominated in wei
+contract DegenButton is Ownable {
+    struct Parameters {
+        uint128 fee;        // fee per button press (in Gwei)
+        uint64 blockDelay;  // number of blocks that must pass before a prize is claimed
+        uint16 houseCut;    // percent of the pool set aside for the DAO
+        uint16 seedCut;     // percent of the rewards set aside for seeding the next round
+    }
+
+    struct Balances {
+        uint prizePool;     // prize pool to be distributed to the winner
+        uint128 seedFund;   // funds allocated to seed the next round
+        uint128 houseFund;  // the house's cut
+    }
+
+    address lastClicked;    // initialized to 0x0 address
+    uint lastBlockNumber;   // initial value doesn't matter since only lastClicked can claimTreasure()
+    Balances balances;      // self explanatory
+    Parameters currParams;  // current value of the game's parameters
+    Parameters nextParams;  // parameters values to update to in the next round
     
-    modifier onlyLastSeen {
-        require(msg.sender == lastSeen, "nice try, but no");
+    modifier onlyLastClicked {
+        require(msg.sender == lastClicked, "nice try, but no");
         _;
     }
 
     // bad UX to allow the same user to press button twice in a row
-    modifier notLastSeen { 
-        require(msg.sender != lastSeen, "already pressed");
+    modifier notLastClicked { 
+        require(msg.sender != lastClicked, "already pressed");
         _;
     }
 
-    constructor(uint _fee) {
+    constructor(uint128 _fee, uint64 _blockDelay, uint16 _houseCut, uint16 _seedCut) {
         console.log("Deploying a Button with fee: ", _fee);
-        lastBlockNumber = block.number;
-        fee = _fee;
+        nextParams.fee = _fee;
+        nextParams.blockDelay = _blockDelay;
+        nextParams.houseCut = _houseCut;
+        nextParams.seedCut = _seedCut;
+        updateParams();
     }
     
-    function pressButton() external payable notLastSeen {
-        require(msg.value == fee, "Incorrect fee sent");
+    
+    /*************************
+     * PLAYER INTERACTIONS
+     *************************/
+    
+    /*
+     * @dev presses the button by sending fee, grants the clicker exclusive
+     * rights to claim prize, until they are griefed by the next clicker
+     */
+    function pressButton() external payable notLastClicked {
+        require(msg.value > currParams.fee, "Insufficient fee sent");
+        uint128 seedInc = uint128(currParams.seedCut * msg.value);
+        uint128 houseInc = uint128(currParams.houseCut * msg.value);
+        balances.seedFund += seedInc;
+        balances.houseFund += houseInc;
+        balances.prizePool += (msg.value - seedInc - houseInc);
         lastBlockNumber = block.number;
-        lastSeen = msg.sender;
+        lastClicked = msg.sender;
     }
 
-    function claimTreasure() external onlyLastSeen {
-        require(block.number - lastBlockNumber > 3, "Patience is bitter, but its fruit is sweet");  // no safe checks required given guarantees the block number is monotonically increasing
-        (bool success, ) = msg.sender.call{value: address(this).balance}('');                       // // favor call rather than transfer/send to avoid gas-based failures; no need to gaurd against reentrancy since full balance is withdrawn anyways
+    /*
+     * @dev claims the prize pool, access only granted if the caller is the
+     * last clicker and enough blocks have been mined
+     */
+    function claimTreasure() external payable onlyLastClicked {
+        require(
+            block.number - lastBlockNumber >= currParams.blockDelay,
+            "Patience is bitter, but its fruit is sweet"
+        );     // block number should be monotonically increasing
+        lastClicked = address(0);
+        (bool success, ) = msg.sender.call{value: balances.prizePool}('');  // is there a need to gaurd against reentrancy?
         require(success);
-        lastSeen = address(0);
+        updateParams();
     }
 
-    function getBalance() public view returns (uint) {
-        return address(this).balance;
+    function seedCurrentRound() external payable {
+        balances.prizePool += msg.value;
+    }
+
+
+    /*************************
+     * INTERNAL FUNCTIONS
+     *************************/
+
+    /*
+     * @dev updates the current parameters to reflect the ones queued
+     *  leaves nextParams unchanged
+     */
+    function updateParams() internal {
+        currParams.fee = nextParams.fee;
+        currParams.blockDelay = nextParams.blockDelay;
+        currParams.houseCut = nextParams.houseCut;
+        currParams.seedCut = nextParams.seedCut;
     }
 }
 
